927ffb493d95790f41c2b17d394257f32885203f
Holy-Knight <vnycall74@naver.com>
2017-05-23 23:44:55 +0900

    한글화 100%

---
+++ GMS2-KoreanPatch.bat
@@ -0,0 +1,34 @@
+@echo off
+
+echo ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+echo ┃      게임메이커 스튜디오 2 한글 패치기       ┃
+echo ┣━━━━━━━━━━━━━━━━━━━━━━━┫
+echo ┃                                              ┃
+echo ┃            1. 일반판 한글 패치               ┃
+echo ┃                                              ┃
+echo ┃            2. 스팀판 한글 패치               ┃
+echo ┃                                              ┃
+echo ┣━━━━━━━━━━━━━━━━━━━━━━━┫
+echo ┃                                              ┃
+echo ┃                한글화 참여자                 ┃
+echo ┃                                              ┃
+echo ┃                                              ┃
+echo ┃                    유광무                    ┃
+echo ┃                                              ┃
+echo ┃                    신동주                    ┃
+echo ┃                                              ┃
+echo ┃                    혀느리                    ┃
+echo ┃                                              ┃
+echo ┃                    백종익                    ┃
+echo ┃                                              ┃
+echo ┃                    김진수                    ┃
+echo ┃                                              ┃
+echo ┃                   맛난 호빵                  ┃
+echo ┃                                              ┃
+echo ┃                     호용                     ┃
+echo ┃                                              ┃
+echo ┣━━━━━━━━━━━━━━━━━━━━━━━┫
+echo ┃                                              ┃
+
+set /p str=메뉴 : 
+pause
