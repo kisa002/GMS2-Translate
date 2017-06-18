3094fdb87e1876b8eab85fd9bb9b25419ef1c9c7
Holy-Knight <vnycall74@naver.com>
2017-06-18 23:30:35 +0900

    한글화 추가

--- GMS2-KoreanPatch.bat
+++ GMS2-KoreanPatch.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:Start
 echo ┏━━━━━━━━━━━━━━━━━━━━━━━┓
 echo ┃      게임메이커 스튜디오 2 한글 패치기       ┃
 echo ┣━━━━━━━━━━━━━━━━━━━━━━━┫
@@ -7,6 +8,8 @@ echo ┃                                              ┃
 echo ┃            1. 일반판 한글 패치               ┃
 echo ┃                                              ┃
 echo ┃            2. 스팀판 한글 패치               ┃
+echo ┃                                              ┃
+echo ┃            3. 커스텀 한글 패치               ┃
 echo ┃                                              ┃
 echo ┣━━━━━━━━━━━━━━━━━━━━━━━┫
 echo ┃                                              ┃
@@ -19,10 +22,6 @@ echo ┃                    신동주                    ┃
 echo ┃                                              ┃
 echo ┃                    혀느리                    ┃
 echo ┃                                              ┃
-echo ┃                    백종익                    ┃
-echo ┃                                              ┃
-echo ┃                    김진수                    ┃
-echo ┃                                              ┃
 echo ┃                   맛난 호빵                  ┃
 echo ┃                                              ┃
 echo ┃                     호용                     ┃
@@ -30,5 +29,16 @@ echo ┃                                              ┃
 echo ┣━━━━━━━━━━━━━━━━━━━━━━━┫
 echo ┃                                              ┃
 
-set /p str=메뉴 : 
+set /p menu=메뉴 번호 : 
+
+if %menu% == 1 (
+   copy korean.csv "C:\Program Files\GameMaker Studio 2\Languages\korean.csv"
+) else if %menu% == 2 (
+   @echo 스팀
+) else (
+   @echo 없는 메뉴입니다!
+   pause
+   cls
+   goto Start
+)
 pause
